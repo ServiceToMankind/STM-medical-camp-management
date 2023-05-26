e1b00887ea749c57b675177886f7ea471a0a151c
kiranpranay <pranaybannu12@gmail.com>
2023-05-26 20:57:31 +0530

    commit name

--- verify_patients.php
+++ verify_patients.php
@@ -14,91 +14,93 @@ if(isset($_GET['pquery'])){
 }
 ?>
 <!-- partial -->
-<div class="main-panel">
-    <div class="content-wrapper">
-        <div class="page-header">
-            <h3 class="page-title">
-                <span class="page-title-icon bg-gradient-danger text-white me-2">
-                    <i class="mdi mdi-home"></i>
-                </span>
-                Add Patients
-            </h3>
-            <nav aria-label="breadcrumb">
-                <ul class="breadcrumb">
-                    <li class="breadcrumb-item active" aria-current="page">
-                        <span></span>Overview
-                        <i class="mdi mdi-alert-circle-outline icon-sm text-primary align-middle"></i>
-                    </li>
-                </ul>
-            </nav>
-        </div>
-        <!-- form  -->
-        <div class="col-md-6 grid-margin stretch-card">
-            <div class="card">
-                <div class="card-body">
-                    <h4 class="card-title">Add Patients</h4>
-                    <p class="card-description">Data must be verified.</p>
-                    <form class="forms-sample" method="GET">
-                        <div class="form-group">
-                            <label for="pquery">Mobile No.</label>
-                            <input type="text" class="form-control" name="pquery" id="pquery" placeholder="Username" />
-                        </div>
-                        <button type="submit" class="btn btn-gradient-primary me-2">
-                            Fetch
-                        </button>
-                    </form>
-
-                    <div class="mobile_checked">
-
+<div class="content-wrapper">
+    <div class="page-header">
+        <h3 class="page-title">
+            <span class="page-title-icon bg-gradient-danger text-white me-2">
+                <i class="mdi mdi-home"></i>
+            </span>
+            Add Patients
+            <span class="page-title-icon bg-gradient-danger text-white me-2">
+                <i class="mdi mdi-home"></i>
+            </span>
+        </h3>
+        <nav aria-label="breadcrumb">
+            <ul class="breadcrumb">
+                <li class="breadcrumb-item active" aria-current="page">
+                    <span></span>Overview
+                    <i class="mdi mdi-alert-circle-outline icon-sm text-primary align-middle"></i>
+                </li>
+            </ul>
+        </nav>
+    </div>
+    <!-- form  -->
+    <div class="col-md-6 grid-margin stretch-card">
+        <div class="card">
+            <div class="card-body">
+                <h4 class="card-title">Add Patients</h4>
+                <p class="card-description">Data must be verified.</p>
+                <form class="forms-sample" method="GET">
+                    <div class="form-group">
+                        <label for="pquery">Mobile No.</label>
+                        <input type="text" class="form-control" name="pquery" id="pquery" placeholder="Username" />
                     </div>
-                    <div class="row">
-                        <div class="col-12 grid-margin">
-                            <div class="card">
-                                <div class="card-body">
-                                    <h4 class="card-title">Patient Records</h4>
-                                    <div class="table-responsive">
+                    <button type="submit" class="btn btn-gradient-primary me-2">
+                        Fetch
+                    </button>
+                </form>
+
+                <div class="mobile_checked">
+
+                </div>
+                <div class="row">
+                    <div class="col-12 grid-margin">
+                        <div class="card">
+                            <div class="card-body">
+                                <h4 class="card-title">Patient Records</h4>
+                                <div class="table-responsive">
 
 
-                                        <?php 
+                                    <?php 
                     if(isset($data) ){
                         if(isset($data['status'])){
                             echo "<h3>No Records Found</h3>";
                         echo "<h4><a href=manage_patients"."?mobile=".$pquery.">click here to register</a></h4>";
                         }else{
                             ?>
-                                        <table class="table">
-                                            <thead>
-                                                <tr>
-                                                    <th>Id</th>
-                                                    <th>Name</th>
-                                                    <th>Age</th>
-                                                </tr>
-                                            </thead>
-                                            <tbody>
-                                                <?php
+                                    <table class="table">
+                                        <thead>
+                                            <tr>
+                                                <th>Id</th>
+                                                <th>Name</th>
+                                                <th>Age</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <?php
                             foreach ($data as $row) {
                                 ?>
-                                                <tr>
-                                                    <td><?php echo $row['id']; ?></td>
-                                                    <td><?php echo $row['name']; ?></td>
-                                                    <td><?php echo $row['age']; ?></td>
-                                                </tr>
-                                                <?php
+                                            <tr>
+                                                <td><?php echo $row['id']; ?></td>
+                                                <td><?php echo $row['name']; ?></td>
+                                                <td><?php echo $row['age']; ?></td>
+                                            </tr>
+                                            <?php
                         } } } ?>
-                                            </tbody>
-                                        </table>
-                                    </div>
+                                        </tbody>
+                                    </table>
                                 </div>
                             </div>
                         </div>
                     </div>
-
-
                 </div>
+
+
             </div>
         </div>
     </div>
-    <!-- content-wrapper ends -->
-    <?php
+</div>
+<!-- content-wrapper ends -->
+<?php
 include 'includes/footer.php';
 ?>
